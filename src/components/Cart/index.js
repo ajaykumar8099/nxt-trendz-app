994543279a559512f9a57eb6ee1767f329bf7b3b
ajaykumar <ajaykumarthappeta119@gmail.com>
2023-11-17 07:20:27 +0000

    nxt final code

---
+++ src/components/Cart/index.js
@@ -0,0 +1,32 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+import EmptyCartView from '../EmptyCartView'
+
+import CartContext from '../../context/CartContext'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const showEmptyValue = cartList.length === 0
+      return (
+        <>
+          <Header />
+          {showEmptyValue ? (
+            <EmptyCartView />
+          ) : (
+            <div className="cart-container">
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <CartListView />
+              </div>
+            </div>
+          )}
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
